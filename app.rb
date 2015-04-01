45e63ead8d6e0f5a9858b929134e5cabb7fa56d7
Jesse Shawl <jesse@jshawl.com>
2015-04-01 19:11:59 -0400

    add create functionality

--- app.rb
+++ app.rb
@@ -12,7 +12,8 @@ loop do
   puts "Welcome! Choose 1 of the following:"
   puts "1 - Play with all flashcards"
   puts "2 - View all categories"
-  puts "3 - Exit."
+  puts "3 - Create a new flashcard"
+  puts "9 - Exit."
   case gets.chomp
   when "1"
     @cards = Flashcard.all
@@ -23,6 +24,20 @@ loop do
     @cards = Flashcard.where( category_id: input )
     game.play @cards
   when "3"
+    puts "Creating new flashcard:"
+    puts "Enter question:"
+    question = gets.chomp
+    puts "Enter answer:"
+    answer = gets.chomp
+    puts "Enter category:"
+    category = Category.find_or_create_by( title: gets.chomp )
+    @flashcard = Flashcard.new( question: question, answer: answer, category: category )
+    if @flashcard.save
+      puts "Flashcard created successfully!"
+    else
+      puts "Error creating flashcard"
+    end
+  when "9"
     puts "bye."
     break
   end
